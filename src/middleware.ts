895834e19ed52f2e78af41298def17077ad05a51
Suchismita Bose <133328192+psychomita@users.noreply.github.com>
2025-05-02 15:00:14 +0530

    feat: add middleware for session management and route protection

---
+++ src/middleware.ts
@@ -0,0 +1,48 @@
+import type { auth } from "@/lib/auth";
+import { betterFetch } from "@better-fetch/fetch";
+import { NextRequest, NextResponse } from "next/server";
+
+type Session = typeof auth.$Infer.Session;
+
+const authRoutes = ["/login", "/register"];
+const passwordRoutes = ["/reset-password", "/forgot-password"];
+const protectedRoutes = ["/dashboard", "/settings", "/profile"];
+
+export default async function middleware(request: NextRequest) {
+  const pathName = request.nextUrl.pathname;
+  const isAuthRoute = authRoutes.includes(pathName);
+  const isPasswordRoute = passwordRoutes.includes(pathName);
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    pathName.startsWith(route),
+  );
+
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get("cookie") || "", // Forward cookies from the request
+      },
+    },
+  );
+
+  if (!session) {
+    if (isAuthRoute || isPasswordRoute) {
+      return NextResponse.next();
+    }
+    if (isProtectedRoute) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+    return NextResponse.next();
+  }
+
+  if (isAuthRoute || isPasswordRoute) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
+};
